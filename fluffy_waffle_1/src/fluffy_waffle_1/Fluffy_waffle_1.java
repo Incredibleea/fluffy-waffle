0f6232d5f6f2f53f18c1c24e22ac3e72202e42ad
nowak_1105010 <nowak_1105010@a07>
2017-10-02 12:49:33 +0200

    Initial af

---
+++ fluffy_waffle_1/src/fluffy_waffle_1/Fluffy_waffle_1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+Samolot
+odleglosc
+spalania
+ilosc miejsc
+liczymy zysk
+*/
+
+package fluffy_waffle_1;
+
+/**
+ *
+ * @author nowak_1105010
+ */
+public class Fluffy_waffle_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
