fac392d27b9e4ed3b0e7aacd66f574bf27867376
nowak_1105010 <nowak_1105010@a07>
2017-10-02 13:26:40 +0200

    Plane income

--- fluffy_waffle_1/src/fluffy_waffle_1/Fluffy_waffle_1.java
+++ fluffy_waffle_1/src/fluffy_waffle_1/Fluffy_waffle_1.java
@@ -4,27 +4,54 @@
  * and open the template in the editor.
  */
 
-/*
-Samolot
-odleglosc
-spalania
-ilosc miejsc
-liczymy zysk
-*/
-
 package fluffy_waffle_1;
 
 /**
  *
  * @author nowak_1105010
  */
-public class Fluffy_waffle_1 {
 
-    /**
-     * @param args the command line arguments
-     */
+interface flightInterface {
+    double calculateIncome();
+}
+
+class Plane
+{
+    double crewSalary = 4800.12;
+    int litersPer100km = 380;
+    int maxPassengersNo = 250;
+    int crewNo = 10;
+}
+
+class Flight implements flightInterface {
+    int passengersNo;
+    int distance;
+    int ticketPrice;
+    Plane plane;
+    
+    @Override
+    public double calculateIncome() {      
+        return passengersNo * ticketPrice - calculateCosts();
+    }
+    
+    private double calculateCosts() {
+        return (distance / 100) * plane.litersPer100km * 4.69 + plane.crewNo * plane.crewSalary;
+    }
+    
+    public Flight(int pN, int d, int tP) {
+        plane = new Plane();
+        passengersNo = pN;
+        distance = d;
+        ticketPrice = tP;
+    }
+}
+
+public class Fluffy_waffle_1 {
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        Flight flight = new Flight(120, 1000, 1000);
+        System.out.println("Income: " + flight.calculateIncome());
     }
     
 }
